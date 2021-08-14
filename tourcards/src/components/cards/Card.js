7a0beb8da01c7dd005f06b2f3714f713a36aa428
E-Ozdemir <emreclarusway@gmail.com>
2021-08-14 15:44:12 +0200

    card compüonent added

---
+++ tourcards/src/components/cards/Card.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { data } from '../helper/Data';
+import "./Card.css"
+
+function Card() {
+    return (
+        <div className="card-container">
+            {data.map((card)=>{
+                return(
+                    <div className='cards'>
+                        <div className="title">
+                            <h2>{card.title}</h2>
+                        </div>
+                        <img src={card.image}/>
+                        <div className="card-over">
+                            <p>{card.desc}</p>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Card
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className="card-container">
+<div className="cards">
+    <div className="title"><h3>{data[0].title}</h3></div>
+
+    <img  src={data[0].image} alt="#"/>
+    <div className="card-over"><p>{data[0].desc}</p></div>
+</div>
+</div> */}

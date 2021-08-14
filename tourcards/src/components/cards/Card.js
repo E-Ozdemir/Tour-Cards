1a25136976920d292c904326f13efbe41c5afeba
E-Ozdemir <emreclarusway@gmail.com>
2021-08-14 17:01:21 +0200

    last changes

--- tourcards/src/components/cards/Card.js
+++ tourcards/src/components/cards/Card.js
@@ -24,6 +24,53 @@ function Card() {
 
 export default Card
 
+{/* <div className="App">
+{
+  data.map(item =><Card img={item.image} desc={item.desc} title={item.title}/>)
+}
+</div> */}
+
+
+
+////tiklamayla yaziyi gizleme
+// import React, { useState } from "react";
+// import { data } from "../../helpers/Data";
+// import "./Card.css";
+// function Card() {
+//   const [showImage, setShowImage] = useState(true);
+//   return (
+//     <div className="card-container" onClick={() => setShowImage(!showImage)}>
+//       {showImage ? (
+//         <>
+//           {data.map((card) => {
+//             return (
+//               <div className="cards" key={card.id}>
+//                 <img src={card.image} alt={card.title} />
+//               </div>
+//             );
+//           })}
+//         </>
+//       ) : (
+//         <>
+//           {data.map((card) => {
+//             return (
+//               <div className="cards" key={card.id}>
+//                 <div className="title">
+//                   <h2>{card.title}</h2>
+//                 </div>
+//                 <img src={card.image} alt={card.title} />
+//                 <div className="card-over">
+//                   <p>{card.desc}</p>
+//                 </div>
+//               </div>
+//             );
+//           })}
+//         </>
+//       )}
+//     </div>
+//   );
+// }
+// export default Card;
 
 
 
